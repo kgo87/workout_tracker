6865bade717640ec031b2b53c457afe039c3bfa8
Kat Gomozova <kathy.gomozova@gmail.com>
2021-02-11 11:19:10 -0800

    deploy

--- server.js
+++ server.js
@@ -13,6 +13,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout_db", {
   useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
   useFindAndModify: false
 });
 
